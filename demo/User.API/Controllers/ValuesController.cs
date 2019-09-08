589485d8603020b68b93a7700b22cec90de5d20b
limengjie <li.mengjie@yalla.live>
2019-09-08 22:05:01 +0800

    添加User.API

---
+++ demo/User.API/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using User.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace User.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private UserContext _useContext;
+        public ValuesController(UserContext userContext)
+        {
+            _useContext = userContext;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return _useContext.Users.SingleOrDefault(x => x.Name == "jack.li").Name;
+        }
+    }
+}
---
+++ demo/User.API/Data/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User.API.Models;
+
+namespace User.API.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>()
+                .ToTable("Users")
+                .HasKey(u => u.Id);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
