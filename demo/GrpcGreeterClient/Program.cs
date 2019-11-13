d47c759783fdf62965a54ebd4eef8c14ab05e28d
limengjie <li.mengjie@yalla.live>
2019-11-13 16:33:55 +0800

    add grpc demo

---
+++ demo/GrpcGreeterClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    /// <summary>
+    /// Install-Package Grpc.Net.Client
+    /// Install-Package Google.Protobuf
+    /// Install-Package Grpc.Tools
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "World By GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
