a7e0cc7031fbe98397e1b43a813a7a3e1e31d111
limengjie <li.mengjie@yalla.live>
2019-11-14 13:26:09 +0800

    add model

--- demo/User.API/Data/UserContext.cs
+++ demo/User.API/Data/UserContext.cs
@@ -19,6 +19,22 @@ namespace User.API.Data
             modelBuilder.Entity<AppUser>()
                 .ToTable("Users")
                 .HasKey(u => u.Id);
+
+            modelBuilder.Entity<UserProperty>().Property(x => x.Key).HasMaxLength(100);
+            modelBuilder.Entity<UserProperty>().Property(x => x.Value).HasMaxLength(100);
+            modelBuilder.Entity<UserProperty>()
+                .ToTable("UserProperties")
+                .HasKey(u => new { u.Key, u.AppUserId, u.Value });
+
+            modelBuilder.Entity<UserTage>().Property(x => x.Tag).HasMaxLength(100);
+            modelBuilder.Entity<UserTage>()
+                .ToTable("UserTages")
+                .HasKey(u => new { u.UserId, u.Tag });
+
+            modelBuilder.Entity<BpFile>()
+               .ToTable("UserTages")
+               .HasKey(u => u.Id);
+
             base.OnModelCreating(modelBuilder);
         }
 
