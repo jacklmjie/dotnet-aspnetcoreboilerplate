f0c0db9a56bb33c311bb2a6b32c5b8b7b4b8b859
limengjie <li.mengjie@yalla.live>
2020-06-15 10:45:41 +0800

    add

---
+++ demo/SkyAPM/sampleapp2/Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace sampleapp2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var client = new HttpClient();
+            return await client.GetStringAsync("http://localhost:5001/weatherforecast");
+        }
+    }
+}
